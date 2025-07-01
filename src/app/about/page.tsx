590e42780482a7d554fb28c008786481c846891d
Issam El Jaouhari <Issamefar58@gmail.com>
2025-07-01 20:53:01 +0100

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About(){
+    return <h1 className="hello">HEEYY ABOUT</h1>
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function Profile(){
+    return <h1 className="">HEEYY Profile Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam reprehenderit debitis, eos amet velit optio quaerat, nemo neque quidem architecto doloribus, nam molestiae eaque? Quo quod at fugiat culpa pariatur.</h1>
+}
